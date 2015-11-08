4bb386fee8c95bc484c353dbe8c5a6a5971bd079
Philippe Lafrance <phil.laf@gmail.com>
2015-11-08 16:59:21 -0500

    No longer including controller in this plugin

--- config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Cake\Routing\Router;
-
-Router::scope('/', ['plugin' => 'Posts'], function ($routes) {
-
-    $routes->connect('/posts', ['controller' => 'Posts']);
-    $routes->connect('/posts/:action/*', ['controller' => 'Posts']);
-    $routes->fallbacks('DashedRoute');
-});
